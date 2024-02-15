e2a0bee33f25b27b4bd505ad4f0d08dca5935c03
Ivann4y <nayakaivanap@gmail.com>
2024-02-16 00:49:18 +0700

    Update website galeri foto 2

--- resources/views/galeri/index.blade.php
+++ resources/views/galeri/index.blade.php
@@ -5,6 +5,6 @@
     <h1 align="center">{{ $user->email }}</h1>
     <h1 align="center">{{ $user->alamat }}</h1>
     <a class="bg-gray-700 rounded-full fixed text-white grid place-items-center text-4xl w-16 h-16 bottom-5 right-5" href="/newImage">
-        <p class="mb-1">+</p>
+        <p class="mb-2">+</p>
     </a>
 @endsection
